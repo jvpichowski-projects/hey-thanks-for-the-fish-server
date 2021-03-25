7ac543d675cde0c150ea71ed8d932de0237d095b
Jan von Pichowski <janvonpichowski@gmail.com>
2021-03-25 22:13:11 +0100

    Updated base protocol

---
+++ src/main/java/de/jvpichowski/htftf/protocol/base/MoveRequest.java
@@ -0,0 +1,6 @@
+package de.jvpichowski.htftf.protocol.base;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+@XStreamAlias("sc.framework.plugins.protocol.MoveRequest") //this alias did not exist back in 2015
+public interface MoveRequest { }
